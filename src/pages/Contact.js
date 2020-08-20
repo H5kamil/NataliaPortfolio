928980ec3f925fd3660e658a8c993209d44efcbf
Kamil Nowak <kamil.nowak.p.lodzka@gmail.com>
2020-08-20 13:16:41 +0200

    added sending email connection

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import nodemailer from 'nodemailer';
 import SocialMedia from '../components/SocialMedia';
 import Footer from '../components/Footer';
+import axios from 'axios';
 import '../styles/Contact.css';
 import '../App.css';
 
@@ -11,7 +11,8 @@ class Contact extends Component {
     name: '',
     email: '',
     subject: '',
-    message: ''
+    message: '',
+    validEmial: true
   }
 
   onChangeName = (e) => {
@@ -38,43 +39,40 @@ class Contact extends Component {
     })
   }
 
-  sendMail = async () => {
-    // Generate test SMTP service account from ethereal.email
-    // Only needed if you don't have a real mail account for testing
-    console.log("testacc ")
-    let testAccount = await nodemailer.createTestAccount();
-    console.log("testacc passed")
-  
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: testAccount.user, // generated ethereal user
-        pass: testAccount.pass, // generated ethereal password
-      },
-    });
-  
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-      from: this.state.email.toString(), // sender address
-      to: "kamil.nowak.p.lodzka@gmail.com", // list of receivers
-      subject: this.state.subject.toString(), // Subject line
-      text: this.state.message.toString(), // plain text body
-      html: "<b>Hello world?</b>", // html body
-    });
-  
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-  
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  emailIsValid = (email) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
   }
 
-  onClickSend = () => {
-    this.sendMail();
+  onClickSend = async () => {
+    console.log(this.state.subject);
+    if(!this.emailIsValid(this.state.email)){
+      alert("Email is not valid")
+    }else if (this.state.subject === '' || this.state.message === '' ) {
+      alert("Fill required field")
+    }else {
+      axios.defaults.headers = {
+        'Content-Type': 'application/json'
+      }
+  
+      const data = {
+        email: this.state.email,
+        text: "Portfolio contact form",
+        subject: this.state.subject,
+        message: this.state.message
+      }
+      const url = 'https://send-gmail-api.herokuapp.com/api/send';
+      try {
+        const response = await axios.post(url, data); 
+        console.log(response);
+        if(response.status === 200) {
+          alert("Email sent");
+        }else {
+          alert("Error occured");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
   }
 
   render() {
