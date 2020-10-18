28fe5b61235ecafa46b5b750c98b807e71c1b90e
Kamil Nowak <kamil.nowak.p.lodzka@gmail.com>
2020-10-18 17:21:32 +0200

    watercolor portraits finished

--- src/pages/Watercolor.js
+++ src/pages/Watercolor.js
@@ -1,15 +1,60 @@
 import React, { Component } from 'react';
+import SocialMedia from '../components/SocialMedia';
+import Footer from '../components/Footer';
+import Cooperation from '../components/Cooperation';
+import NextProject from '../components/NextProject';
+import { images } from "./images";
+import '../styles/Watercolor.css';
+import '../App.css';
 
 class Watercolor extends Component {
+
+  state = {
+    images: images
+  }
 
   //scroll top after previous scrolling
   componentDidMount() {
     window.scrollTo(0, 0);
+    console.log(this.state.images);
   }
 
   render() {
+
+    const imgTab = this.state.images;
+    const imgList = imgTab.map((item) => (
+      // eslint-disable-next-line jsx-a11y/alt-text
+      <img key={item.id} src={item.src} alt=""/>
+    ))
+
     return (
-        <>Watercolor</>
+        <>
+          <main className="container">
+            <section className="wat-first-sec">
+              <div>
+                Watercolors and pencil portraits
+              </div>
+              <div>
+                #100 faces challenge
+              </div>
+            </section>
+            <div className="wat-img-container">
+              <div>Design process</div>
+              <div>Case Study</div>
+              <div>Developing my skills in watecolours technique as well as drawing and 
+                sketching. Improving color mixing and use of tones and values more 
+                intentionally. The challenge follow is focused on creating 100 watercolour 
+                portraits. Can't wait to see improvements! :) </div>
+              <section className="wat-cont">
+                {imgList}
+              </section>
+            </div>
+            <NextProject id={3}/>
+            <Cooperation/>
+            <SocialMedia/>
+            <Footer/>
+          </main>
+        </>
     );
   }
 }
---
+++ src/pages/images.js
@@ -0,0 +1,68 @@
+export const images = [
+    {
+      id: 1,  
+      src: require('../water-img/1.png'),
+      width: 1,
+      height: 1
+    },
+    {
+      id: 2,  
+      src: require('../water-img/2.png'),
+      width: 1,
+      height: 1
+    },
+    {
+      id: 3,    
+      src: require('../water-img/3.png'),
+      width: 1,
+      height: 1
+    },
+    {
+      id: 4,  
+      src: require('../water-img/4.png'),
+      width: 2,
+      height: 1
+    },
+    {
+      id: 5,    
+      src: require('../water-img/5.png'),
+      width: 1,
+      height: 1
+    },
+    {
+        id: 6,
+      src: require('../water-img/6.jpg'),
+      width: 2,
+      height: 1
+    },
+    {
+        id: 7,
+      src: require('../water-img/7.png'),
+      width: 1,
+      height: 1
+    },
+    {
+        id: 8,
+      src: require('../water-img/8.png'),
+      width: 1,
+      height: 1
+    },
+    {
+        id: 9,
+      src: require('../water-img/9.png'),
+      width: 1,
+      height: 1
+    },
+    {
+        id: 10,
+      src: require('../water-img/10.png'),
+      width: 1,
+      height: 1
+    },
+    {
+        id: 11,
+      src: require('../water-img/11.png'),
+      width: 1,
+      height: 1
+    }
+  ];
